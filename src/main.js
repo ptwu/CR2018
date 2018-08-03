9b64067e88fb284d36c5b6fc21f05cc54ea2ec31
Pacnet Netty <bhegde8@gmail.com>
2018-08-03 05:00:29 -0400

    Basic google sign-in code added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 var {Component} = React;
 
+var provider = new firebase.auth.GoogleAuthProvider();
+var user = null;
+
 class Header extends Component {
     render() {
         return(
@@ -9,8 +12,6 @@ class Header extends Component {
             
                     <ul className="pure-menu-list">
                         <li className="pure-menu-item pure-menu-selected"><a href="#" className="pure-menu-link">Home</a></li>
-                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Tour</a></li>
-                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Sign Up</a></li>
                     </ul>
                 </div>
             </div> 
@@ -21,14 +22,14 @@ class Header extends Component {
 class Splash extends Component {
     render() {
         return (
-            <div class="splash-container">
-                <div class="splash">
-                    <h1 class="splash-head">Big Bold Text</h1>
-                    <p class="splash-subhead">
+            <div className="splash-container">
+                <div className="splash">
+                    <h1 className="splash-head">Big Bold Text</h1>
+                    <p className="splash-subhead">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                     </p>
                     <p>
-                        <a href="http://purecss.io" class="pure-button pure-button-primary">Get Started</a>
+                        <a href="http://purecss.io" className="pure-button pure-button-primary">Get Started</a>
                     </p>
                 </div>
             </div>
@@ -39,26 +40,99 @@ class Splash extends Component {
 class GridSystem extends Component {
     render() {
         return (
-            <div class="pure-g grid-example">
-                <div class="pure-u-1 ghost-wrap">
-                    <div class="pure-g-r">
-                        <div class="pure-u-1-3">
-                        <div id="grid_left_box" class="boxy">
+            <div className="pure-g grid-example">
+                <div className="pure-u-1 ghost-wrap">
+                    <div className="pure-g-r">
+                        <div className="pure-u-1-3">
+                        <div id="grid_left_box" className="boxy">
                             
                         </div>
                         </div>
-                        <div class="pure-u-1-3">
-                        <div id="grid_middle_box" class="boxy">
+                        <div className="pure-u-1-3">
+                        <div id="grid_middle_box" className="boxy">
                             
                         </div>
                         </div>
-                        <div class="pure-u-1-3">
-                        <div id="grid_right_box" class="boxy">
+                        <div className="pure-u-1-3">
+                        <div id="grid_right_box" className="boxy">
                             
                         </div>
                         </div>
                     </div>
                 </div>
+            </div>
+        );
+    }
+}
+
+
+
+class GoogleLogin extends Component {
+
+    googleSignIn() {
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            user = result.user;
+            
+            if(!user.email.endsWith("@wwprsd.org")) {
+                
+                toast("times", "red", "You must sign in with your school (wwprsd.org) Google account.");
+                user = null;
+
+                firebase.auth().signOut().then(function() {
+                    console.log("Signed out of non-wwprsd.org account successfully");
+                }).catch(function(error) {
+                    console.log("Failed to sign out of non-wwprsd.org account -> " + error);
+                });
+
+                return;
+            }
+            console.log(user.email);
+            toast("check", "green", "Success! Logged in as " + user.email);
+
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+    }    
+
+    render() {
+        return (
+            <div className="classreveal_site_content is-center" id="content">
+                <h2 className="content-head">Sign in with Google</h2>
+                <h4 className="content-head">You must use your wwprsd.org account</h4>
+                <button className="content-head" onClick={this.googleSignIn}><img src="img/google_signin/normal.png"/></button>    
+            </div>
+        );
+    }
+}
+
+class PrivacyDisclaimer extends Component {
+    render() {
+        return (
+            <div className="classreveal_site_content is-center" id="content">
+                <p className="content-head">By using ClassReveal, you acknowledge that <br/>anyone with a wwprsd.org Google account can see which classes you are in.</p>
+                <p className="content-head">We do not support non WW-P schools because of security issues.<br/> This year, we are required to verify that ClassReveal users are actual students, <br/> which is why you have to sign in using Google. We cannot verify students <br/> from other school districts, so we're limited to WW-P. Sorry for the inconvenience.</p>
+
+            </div>
+        );
+    }
+}
+
+class ToastNotification extends Component {
+    render() {
+        return (
+            <div id="toast">
+                <div id="toast_img_holder"><i id="toast_img_icon" className="fa"></i></div>
+                <div id="toast_desc"></div>
             </div>
         );
     }
@@ -71,16 +145,38 @@ class App extends Component {
                 <div id="header">
                     <Header/>
                 </div>
-                <div class="classreveal_site_content" id="content">
-                    <h2 class="content-head is-center">Sign in with Google</h2>
-                    <GridSystem/>
-                </div>
-                <div class="footer l-box is-center">
+
+                <GoogleLogin/>
+
+                <PrivacyDisclaimer/>
+                
+                <ToastNotification/>
+
+                <div className="footer l-box is-center">
                    footer
                 </div>
+
+                
             </div>
         )
     }
+}
+
+function toast(icon, color, message) {
+    var toastDiv = document.getElementById("toast")
+
+    document.getElementById("toast_img_icon").className = "fa fa-" + icon;
+    document.getElementById("toast_desc").innerHTML = message;
+
+    toastDiv.className = "toast-" + color + " show";
+    
+    setTimeout(
+        function() 
+        { 
+            toastDiv.className = "";
+        }, 
+        5000
+    );
 }
 
 ReactDOM.render(<App/>, document.getElementById('container'));
